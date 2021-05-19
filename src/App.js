2570f9547826bfbb47200d46001833e9b66ce752
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-05-19 15:06:17 -0400

    Installed MUI

--- src/App.js
+++ src/App.js
@@ -3,34 +3,32 @@ import './App.css';
 // Components
 import Dashboard from './components/dashboard/Dashboard';
 import Header from './components/header/Header';
+import Navigation from './components/navigation/Navigation';
 
-const axios = require('axios');
+// const axios = require('axios');
 
 // This is a test api call. Call this in the App function if you want to communicate with the API.
-const test = () => {
-	const newbody = {
-		obiwan: 'Hello there',
-	};
-
-	axios({
-		method: 'POST',
-		url: 'http://localhost:5000/algorithms/getAlgorithm',
-		data: {
-			name: 'bubblesort',
-		},
-		config: {
-			headers: { 'Content-Type': 'application/json' },
-		},
-	}).then((response) => {
-		console.log(response);
-	});
-};
+// const test = () => {
+// 	axios({
+// 		method: 'POST',
+// 		url: 'http://localhost:5000/algorithms/getAlgorithm',
+// 		data: {
+// 			name: 'bubblesort',
+// 		},
+// 		config: {
+// 			headers: { 'Content-Type': 'application/json' },
+// 		},
+// 	}).then((response) => {
+// 		console.log(response);
+// 	});
+// };
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
-			<Dashboard />
+			{/* <Dashboard /> */}
+			<Navigation />
 		</div>
 	);
 }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,9 @@
+import './Navigation.css';
+
+// Material UI components
+
+function Navigation() {
+	return <div className='Navigation'></div>;
+}
+
+export default Navigation;
