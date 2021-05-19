f5078b694d04b6829d492601c3283b0bf3abb19e
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-05-19 14:40:56 -0400

    API call test successful

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import './App.css';
 import Dashboard from './components/dashboard/Dashboard';
 import Header from './components/header/Header';
 
+const axios = require('axios');
+
+// This is a test api call. Call this in the App function if you want to communicate with the API.
+const test = () => {
+	const newbody = {
+		obiwan: 'Hello there',
+	};
+
+	axios({
+		method: 'POST',
+		url: 'http://localhost:5000/algorithms/getAlgorithm',
+		data: {
+			name: 'bubblesort',
+		},
+		config: {
+			headers: { 'Content-Type': 'application/json' },
+		},
+	}).then((response) => {
+		console.log(response);
+	});
+};
+
 function App() {
 	return (
 		<div className='App'>
