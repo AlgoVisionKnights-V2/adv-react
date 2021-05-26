8c426dfd24b194d11cdfada47c9d0f6766c9edc8
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-05-26 19:15:39 -0400

    Organized code

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import './Dashboard.css';
 import algorithms from './algorithmList';
-import {Card} from '@material-ui/core';
 import Algorithm from '../algorithm/Algorithm';
 
 function Dashboard() {
 	return (
 		<div className='Dashboard'>
-			{algorithms.map((algorithm)=>(<Algorithm title={algorithm.title} description={algorithm.description}/>))}	
+			{algorithms.map((algorithm) => (
+				<Algorithm
+					title={algorithm.title}
+					description={algorithm.description}
+				/>
+			))}
 		</div>
 	);
 }
