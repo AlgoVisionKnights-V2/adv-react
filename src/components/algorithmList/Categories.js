d431d39fad4e36c9dd833404a7da9d5f393e0cd6
hsaidi5 <hsaidi@Knights.ucf.edu>
2021-06-22 18:10:25 -0400

    Added descriptions for all categories.

--- src/components/algorithmList/Categories.js
+++ src/components/algorithmList/Categories.js
@@ -4,27 +4,27 @@ const groups = [
 		path: 'sorting',
 		title: 'Sorting',
 		description:
-			'Sorting Algorithms are used to rearrange elements in an array or list. Some of the most commonly used sorting algorithms include bubble sort, merge sort, and quick sort.',
+			'Sorting algorithms are used to rearrange elements in an array or list. Some of the most commonly used sorting algorithms include bubble sort, merge sort, and quick sort.',
 	},
 	{
 		path: 'searching',
 		title: 'Searching',
-		description: 'Yet to be filled.',
+		description: 'Searching algorithms are used to find a specific element within an array. The 2 most known and used searching algorithms are linear and binary search. Other searching algorithms include fibonacci and jump search.',
 	},
 	{
 		path: 'datastructures',
 		title: 'Data Structures',
-		description: 'Yet to be filled.',
+		description: 'Data structures are used to store and manipulate different types of data. Each data structure is beneficial depending on what is needed from the data being stored. Data structures includes arrays, stacks, queues, trees, and linked lists.',
 	},
 	{
 		path: 'graphs',
 		title: 'Graphs',
-		description: 'Yet to be filled.',
+		description: 'Graphs are a type of data structure. They allow for the connection of different nodes to one another. Different graphs include directed and undirected graphs.',
 	},
 	{
 		path: 'trees',
 		title: 'Trees',
-		description: 'Yet to be filled.',
+		description: 'Trees are a nonlinear data structure. Instead, they have a hierarchical structure. Trees are a useful data structure for fast read and writes of data. Some of the most known trees are binary trees, avl trees, and 2-4 trees.',
 	},
 	// {
 	// 	path: 'probabilisticdatastructures',
@@ -34,7 +34,7 @@ const groups = [
 	{
 		path: 'backtracking',
 		title: 'Backtracking',
-		description: 'Yet to be filled.',
+		description: 'Backtracking is an algorithm which finds solutions to a specific problem. This is done by going through different possible solutions and marking which path led to a solution or not. One of the most used examples of backtracking is the nQueens problem.',
 	},
 	// {
 	// 	path: 'linkedlist',
