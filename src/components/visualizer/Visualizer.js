52a1d66ad43bc694fe595aee462b828788ae6e8b
Stefan Werleman <stefanwerleman@knights.ucf.edu>
2021-06-28 16:31:38 -0400

    Upgrade webgl package and imported dijsktra

--- src/components/visualizer/Visualizer.js
+++ src/components/visualizer/Visualizer.js
@@ -2,17 +2,25 @@ import './Visualizer.css';
 import React from 'react';
 
 // Unity Webgl components for Unity test
-import Unity, { UnityContent } from 'react-unity-webgl';
+import Unity, { UnityContext } from 'react-unity-webgl';
 
 function Visualizer({ path }) {
-	const unityContent = new UnityContent(
-		`UnityBuilds/${path}/build.json`,
-		`UnityBuilds/${path}/UnityLoader.js`
-	);
+	// const unityContent = new UnityContent(
+	// 	`UnityBuilds/${path}/build.json`,
+	// 	`UnityBuilds/${path}/UnityLoader.js`
+	// );
+
+	const unityContext = new UnityContext({
+		loaderUrl: `UnityBuilds/${path}/build.loader.js`,
+		dataUrl: `UnityBuilds/${path}/build.data`,
+		frameworkUrl: `UnityBuilds/${path}/build.framework.js`,
+		codeUrl: `UnityBuilds/${path}/build.wasm`,
+	});
 
 	return (
 		<div className='Visualizer'>
-			<Unity className='Unity' unityContent={unityContent} />
+			{/* <Unity className='Unity' unityContent={unityContent} /> */}
+			<Unity className='Unity' unityContext={unityContext} />
 		</div>
 	);
 }
