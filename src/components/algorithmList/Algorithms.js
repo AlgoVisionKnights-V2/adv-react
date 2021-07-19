f70a2d9404230fa2de5d2f27e3e8b7932a0bb1ef
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-07-19 11:08:24 -0400

    Added overview to list and added linear search page

--- src/components/algorithmList/Algorithms.js
+++ src/components/algorithmList/Algorithms.js
@@ -2,18 +2,25 @@
 
 const algorithms = {
 	sorting: [
+		{ name: 'Overview' },
 		{ name: 'Bubble Sort', path: 'bubblesort' },
 		{ name: 'Selection Sort', path: 'selectionsort' },
 		{ name: 'Insertion Sort', path: 'insertionsort' },
 		{ name: 'Quick Sort', path: 'quicksort' },
 		{ name: 'Merge Sort', path: 'mergesort' },
 	],
-	searching: [{ name: 'Binary Search', path: 'binarysearch' }],
+	searching: [
+		{ name: 'Overview' },
+		{ name: 'Linear Search', path: 'linearsearch' },
+		{ name: 'Binary Search', path: 'binarysearch' },
+	],
 	datastructures: [
-		{ name: 'Arrays', path: 'arrays' },
+		{ name: 'Overview' },
+		// { name: 'Arrays', path: 'arrays' },
 		{ name: 'Linked List', path: 'linkedlist' },
 	],
 	graphs: [
+		{ name: 'Overview' },
 		{ name: "Dijkstra's", path: 'dijkstras' },
 		{ name: 'Bellman Ford', path: 'bellmanford' },
 		{ name: 'Breadth First Search', path: 'breadthfirstsearch' },
@@ -21,6 +28,7 @@ const algorithms = {
 		{ name: "Prim's", path: 'prims' },
 	],
 	trees: [
+		{ name: 'Overview' },
 		{ name: 'AVL', path: 'avl' },
 		{ name: 'Binary Search Tree', path: 'binarysearchtree' },
 		{ name: 'Preorder', path: 'preorder' },
@@ -31,7 +39,7 @@ const algorithms = {
 	// 	{ name: 'Bloom Filters', path: 'bloomfilters' },
 	// 	{ name: 'Skip List', path: 'skiplist' },
 	// ],
-	backtracking: [{ name: 'nQueens', path: 'nqueens' }],
+	backtracking: [{ name: 'Overview' }, { name: 'nQueens', path: 'nqueens' }],
 	// linkedlist: [
 	// 	{ name: 'Singly', path: 'singly' },
 	// 	{ name: 'Doubly', path: 'doubly' },
--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -5,25 +5,10 @@ import { Link } from 'react-router-dom';
 // Child Components
 import Algorithm from '../algorithm/Algorithm';
 
-function Category({
-	category,
-	setPage,
-	categories,
-	setAlgoPage,
-	inCategory,
-	algorithms,
-}) {
+function Category({ inCategory, algorithms }) {
 	// const description = category.description
 	return (
 		<div className='Category'>
-			<Algorithm
-				title={'Overview'}
-				key={'Overview'}
-				width={'100%'}
-				height={280}
-				inCategory={inCategory}
-			/>
-
 			{algorithms.map((algorithm) => (
 				<Link className='Link' to={algorithm.path}>
 					<Algorithm
