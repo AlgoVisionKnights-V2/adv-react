a5feb520bb3e8aa76132e44d3e0996baf32c4b81
Billy Quiroga <bquiroga10@gmail.com>
2022-03-31 15:34:47 -0400

    Update nqueens.js

--- src/visualizers/nqueens/nqueens.js
+++ src/visualizers/nqueens/nqueens.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as d3 from "d3";
 import "./nqueens.css";
+import "../css/button.css";
 
 
 
@@ -343,11 +344,11 @@ export default class Queens extends React.Component {
     return (
       <div>
         <div class="center-screen" id="banner">
-          <button onClick={this.play}>Play</button>
-          <button onClick={this.pause}>Pause</button>
-          <button onClick={this.restart}>Restart</button>
-          <button onClick={this.backward}>&lt;</button>
-          <button onClick={this.forward}>&gt;</button>
+          <button class="button" onClick={this.play}>Play</button>
+          <button class="button" onClick={this.pause}>Pause</button>
+          <button class="button" onClick={this.restart}>Restart</button>
+          <button class="button" onClick={this.backward}>Step Backward</button>
+          <button class="button" onClick={this.forward}>Step Forward</button>
         </div>
         <div class="center-screen" id="message-pane"><span id="message"><h1>Welcome to nQueens!</h1></span></div>
         <div ref={this.ref} class="center-screen"></div>
