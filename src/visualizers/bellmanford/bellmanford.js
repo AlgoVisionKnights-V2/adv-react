f5f8cd88c77d02c85552c9c35b761c83445ce951
Billy Quiroga <bquiroga10@gmail.com>
2022-03-31 15:31:16 -0400

    Update bellmanford.js

--- src/visualizers/bellmanford/bellmanford.js
+++ src/visualizers/bellmanford/bellmanford.js
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 import "./bellmanford.css";
 import createDefaultGraph from "../../foundation/graph/CreateDefaultGraph";
 import Number from "../../foundation/Number";
+import "../css/button.css";
 
 // returns a random number in the range [lo, hi)
 function randInRange(lo, hi) {
@@ -385,11 +386,11 @@ export default class BellmanFord extends React.Component {
     return (
       <div>
         <div class="center-screen">
-          <button onClick={this.play}>Play</button>
-          <button onClick={this.pause}>Pause</button>
-          <button onClick={this.restart}>Restart</button>
-          <button onClick={this.backward}>&lt;</button>
-          <button onClick={this.forward}>&gt;</button>
+          <button class="button" onClick={this.play}>Play</button>
+          <button class="button" onClick={this.pause}>Pause</button>
+          <button class="button" onClick={this.restart}>Restart</button>
+          <button class="button" onClick={this.backward}>Step Backward</button>
+          <button class="button" onClick={this.forward}>Step Forward</button>
         </div>
         <div class="center-screen">
           <span id="message">
